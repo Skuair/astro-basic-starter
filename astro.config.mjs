280a4040cae42b0b2ffe1b99a0b9b89e5a8319f7
Romain <49756503+Skuair@users.noreply.github.com>
2026-02-05 11:54:45 +0100

    Updated dependencies + custom theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-simple-starter.netlify.app/",
-  integrations: [sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  devToolbar: {
-    enabled: false
-  }
+    site: "http://localhost:4321/",
+    integrations: [sitemap()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    devToolbar: {
+        enabled: false,
+    },
 });
